7f4b7347774e9dd9a762a4bd6d70bc8070ea56ef
Magnus <mange.ostling@gmail.com>
2023-10-31 16:33:00 +0100

    first commit

---
+++ frontend/willys-app/src/api/apiRequests.js
@@ -0,0 +1,47 @@
+// api/apiRequests.js
+
+/*
+const API_URL = "https://example.com/api"; // Ersätt detta med din faktiska API URL
+
+
+const apiRequests = {
+  getSomeData: async () => {
+  
+    try {
+      const response = await fetch(`${API_URL}/someEndpoint`);
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("Failed to fetch data");
+      }
+    } catch (error) {
+      throw new Error("Error:", error);
+    }
+  },
+
+  postSomeData: async (requestData) => {
+    try {
+      const response = await fetch(`${API_URL}/postEndpoint`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("Failed to post data");
+      }
+    } catch (error) {
+      throw new Error("Error:", error);
+    }
+  },
+
+  // And other API request functions...
+};
+
+export default apiRequests;
+*/
